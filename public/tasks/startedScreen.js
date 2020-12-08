52b00b8bbecdc2d892feb1b2483456a8609d7f8f
Raymond <raymondsteen4@gmail.com>
2020-12-07 20:23:12 -0500

    fixed start and end screen

--- public/tasks/startedScreen.js
+++ public/tasks/startedScreen.js
@@ -1,25 +1,27 @@
-function drawStarting(txtWidDiv = 20) {
+function drawStarting(txtWidDiv = 50) {
     wid = width * scl;
     hei = height * scl;
     push();
-    ellipse(width, height, 20)
+    // ellipse(width, height, 20)
     noStroke();
     fill(0, 0, 0, 200);
-    rect(-wid / 2, -hei / 2, wid, hei);
-    textSize(min(wid, hei) / txtWidDiv);
+    rect(-wid / 2, -hei / 2, 99999, 99999);
+    textSize(txtWidDiv);
     fill(255);
     textAlign(CENTER, CENTER);
     text("Touch anywhere to play", 0, -50);
     pop();
 }
 
-function drawFinished(txtWidDiv = 20) {
+function drawFinished(txtWidDiv = 50) {
+    wid = width * scl;
+    hei = height * scl;
     push();
     stroke(0);
     strokeWeight(2);
     fill(0, 0, 0, 200);
-    rect(-width / 2, -height / 2, width, height);
-    textSize(min(width, height) / txtWidDiv);
+    rect(-wid / 2, -hei / 2, 99999, 99999);
+    textSize(txtWidDiv);
     fill(0, 255, 0);
     textAlign(CENTER, CENTER);
     text("TASK COMPLETED!", 0, -50);
