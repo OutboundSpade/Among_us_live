ad659c72571932e5ef3e6949ad331c0f0e3257e2
Raymond <raymondsteen4@gmail.com>
2020-12-08 11:51:56 -0500

    Setup host page

--- public/host/main.js
+++ public/host/main.js
@@ -1,12 +1,32 @@
 /// <reference path="../p5.global-mode.d.ts" />
 
+let amongUsFont;
+
+function preload() {
+    amongUsFont = loadFont("../among us font.ttf");
+}
+let socket;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    textFont(amongUsFont);
+
+    socket = io();
+    socket.on('connect', () => {
+        socket.emit("type", "host");
+        console.log("connected to server!");
+    });
 }
+let scl;
 
 function draw() {
     background(0);
-    scale(min(width, height) / 800);
+    push();
+    scl = min(width, height) / 800;
+    translate(width / 2, height / 2);
+    scale(scl);
+    text
+    pop;
 }
 
 function windowResized() {
