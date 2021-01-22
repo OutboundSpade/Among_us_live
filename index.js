4f99d4a6a91571544327baa3b72a83fd4ebb7d8d
Raymond <raymondsteen4@gmail.com>
2021-01-22 12:36:41 -0500

    test signing

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
-const express = require('express');
+const express = require("express");
 const port = 8080;
 
 const app = express();
 
-app.use(express.static('public/'));
+app.use(express.static("public/"));
 
 const server = app.listen(port, function () {
     console.log("Running on Among us live on port " + port);
 });
 
-const socket = require('socket.io');
+const socket = require("socket.io");
 const io = socket(server);
 
 var clients = {};
 var clientsType = {};
-var playerColors = require('./players.json');
+var playerColors = require("./players.json");
 var totalPlayers = Object.keys(playerColors).length;
 var points = 0;
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
     clients[socket.id] = socket;
 
-    socket.on('type', (type) => {
+    socket.on("type", (type) => {
         clientsType[socket.id] = type;
         updateTaskCount();
         showClients("CONNECT");
     });
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         delete clients[socket.id];
         delete clientsType[socket.id];
         updateTaskCount();
         showClients("DISCONNECT");
     });
-    socket.on('resetPoints', () => {
+    socket.on("resetPoints", () => {
         if (clientsType[socket.id] == "host") {
             points = 0;
-            console.log('resetPoints');
+            console.log("resetPoints");
         }
     });
-    socket.on('doneTask', () => {
+    socket.on("doneTask", () => {
         points += 1;
         console.log(`Done Task - ${points} points`);
-        if (getKeyByValue(clientsType, 'host') != undefined) {
-            clients[getKeyByValue(clientsType, 'host')].emit('updatePoints', points);
+        if (getKeyByValue(clientsType, "host") != undefined) {
+            clients[getKeyByValue(clientsType, "host")].emit(
+                "updatePoints",
+                points
+            );
         }
     });
 });
 
 function updateTaskCount() {
-    if (getKeyByValue(clientsType, 'host') != undefined) {
-        clients[getKeyByValue(clientsType, 'host')].emit('updateNumDevices', Object.values(clientsType).filter(type => type != 'host').length);
+    if (getKeyByValue(clientsType, "host") != undefined) {
+        clients[getKeyByValue(clientsType, "host")].emit(
+            "updateNumDevices",
+            Object.values(clientsType).filter((type) => type != "host").length
+        );
     }
 }
 
@@ -57,5 +63,5 @@ function showClients(startStr) {
 }
 
 function getKeyByValue(object, value) {
-    return Object.keys(object).find(key => object[key] === value);
+    return Object.keys(object).find((key) => object[key] === value);
 }
